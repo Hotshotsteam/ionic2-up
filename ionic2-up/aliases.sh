01ac2f49a26c0fd3692c1eccaedd83bfd7167c7e
Darren Gordon <web@darrengordon.net>
2016-05-20 13:02:17 +0000

    Ionic: Tools for ionic and adb commands

---
+++ ionic2-up/aliases.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ ! -f /usr/bin/ionic ]; then
+  echo '#!/bin/sh' > /usr/bin/ionic
+  echo 'docker run --rm -ti --privileged \
+    -v /dev/bus/usb:/dev/bus/usb \
+    -v=/vagrant/project:/project:rw \
+    -p 8100:8100 -p 35792:35792 -p 5037:5037 \
+    ionic2-up:dev \
+    ionic $@' \
+  > /usr/bin/ionic
+  chmod +x /usr/bin/ionic
+fi
+
+if [ ! -f /usr/bin/adb ]; then
+  echo '#!/bin/sh' > /usr/bin/adb
+  echo 'docker run --rm -ti --privileged \
+    -v /dev/bus/usb:/dev/bus/usb \
+    -v=/vagrant/project:/project:rw \
+    -p 8100:8100 -p 35792:35792 -p 5037:5037 \
+    ionic2-up:dev /bin/bash -c ". /root/.bashrc && \
+    adb $@"' \
+  > /usr/bin/adb
+  chmod +x /usr/bin/adb
+fi
