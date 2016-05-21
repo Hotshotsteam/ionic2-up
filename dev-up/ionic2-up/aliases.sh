5c0f9ab31f979c5810a32803d5ebb0bd18adddb1
Darren Gordon <web@darrengordon.net>
2016-05-21 12:34:55 +0000

    Ionic: Fixed livereload port number in ionic alias

--- dev-up/ionic2-up/aliases.sh
+++ dev-up/ionic2-up/aliases.sh
@@ -7,7 +7,7 @@ if [ ! -f /usr/bin/ionic ]; then
   echo "docker run --rm -ti --privileged \
     -v /dev/bus/usb:/dev/bus/usb \
     -v=$PROJECT_PATH:/project:rw \
-    -p 8100:8100 -p 35792:35792 -p 5037:5037 \
+    -p 8100:8100 -p 35729:35729 -p 5037:5037 \
     hotshotsxyz/ionic2-up:dev \
     ionic \$@" \
   > /usr/bin/ionic
@@ -19,7 +19,7 @@ if [ ! -f /usr/bin/adb ]; then
   echo "docker run --rm -ti --privileged \
     -v /dev/bus/usb:/dev/bus/usb \
     -v=$PROJECT_PATH:/project:rw \
-    -p 8100:8100 -p 35792:35792 -p 5037:5037 \
+    -p 8100:8100 -p 35729:35729 -p 5037:5037 \
     hotshotsxyz/ionic2-up:dev /bin/bash -c \". /root/.bashrc && \
     adb \$@\"" \
   > /usr/bin/adb
