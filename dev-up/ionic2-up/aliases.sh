91d0c15fb9937ec1190873f7203234c851e75c7a
Darren Gordon <web@darrengordon.net>
2016-05-22 05:44:22 +0000

    Aliases: use $PWD

--- dev-up/ionic2-up/aliases.sh
+++ dev-up/ionic2-up/aliases.sh
@@ -6,7 +6,7 @@ if [ ! -f /usr/bin/ionic ]; then
   echo '#!/bin/sh' > /usr/bin/ionic
   echo "docker run --rm -ti --privileged \
     -v /dev/bus/usb:/dev/bus/usb \
-    -v=$PROJECT_PATH:/project:rw \
+    -v=\$PWD:/project:rw \
     -p 8100:8100 -p 35729:35729 -p 5037:5037 \
     hotshotsxyz/ionic2-up:dev \
     ionic \$@" \
@@ -18,7 +18,7 @@ if [ ! -f /usr/bin/adb ]; then
   echo '#!/bin/sh' > /usr/bin/adb
   echo "docker run --rm -ti --privileged \
     -v /dev/bus/usb:/dev/bus/usb \
-    -v=$PROJECT_PATH:/project:rw \
+    -v=\$PWD:/project:rw \
     -p 8100:8100 -p 35729:35729 -p 5037:5037 \
     hotshotsxyz/ionic2-up:dev /bin/bash -c \". /root/.bashrc && \
     adb \$@\"" \
