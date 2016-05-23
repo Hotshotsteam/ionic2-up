8c2488ab5ce9182a4376feb1555b2e03a379e6fb
Darren Gordon <web@darrengordon.net>
2016-05-23 09:07:07 +0700

    Aliases: Mount . paths for android to reduce build times and remember RSA key

--- dev-up/ionic2-up/aliases.sh
+++ dev-up/ionic2-up/aliases.sh
@@ -2,10 +2,16 @@
 
 source /etc/profile.d/dev-up.sh
 
+mkdir -p /vagrant/dev-up/temp
+mkdir -p /vagrant/dev-up/temp/.android
+mkdir -p /vagrant/dev-up/temp/.gradle
+
 if [ ! -f /usr/bin/ionic ]; then
   echo '#!/bin/sh' > /usr/bin/ionic
   echo "docker run --rm -ti --privileged \
     -v /dev/bus/usb:/dev/bus/usb \
+    -v /vagrant/dev-up/temp/.android:/root/.android:rw \
+    -v /vagrant/dev-up/temp/.gradle:/root/.gradle:rw \
     -v=\$PWD:/project:rw \
     -p 8100:8100 -p 35729:35729 -p 5037:5037 \
     hotshotsxyz/ionic2-up:dev \
@@ -18,6 +24,7 @@ if [ ! -f /usr/bin/adb ]; then
   echo '#!/bin/sh' > /usr/bin/adb
   echo "docker run --rm -ti --privileged \
     -v /dev/bus/usb:/dev/bus/usb \
+    -v /vagrant/dev-up/temp/.android:/root/.android:rw \
     -v=\$PWD:/project:rw \
     -p 8100:8100 -p 35729:35729 -p 5037:5037 \
     hotshotsxyz/ionic2-up:dev /bin/bash -c \". /root/.bashrc && \
