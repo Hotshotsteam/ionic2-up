9b66db3a185da8801b243834f7378b345c8465e4
Darren Gordon <web@darrengordon.net>
2016-09-01 13:18:50 +0000

    Tailmon: Latest

--- dev-up/tailmon-up.sh
+++ dev-up/tailmon-up.sh
@@ -32,5 +32,4 @@ docker images | awk '{print $1 ":" $2}' | grep 'hotshotsxyz\/tailmon:dev' &>/dev
   docker build -f /vagrant/dev-up/tailmon/tailmon.Dockerfile -t hotshotsxyz/tailmon:dev /vagrant/dev-up/tailmon
 
 # Start tailmon
-echo $FILE_NAME $2 $3
 docker run --name="tailmon" -d -v=$PROJECT_PATH:/project:rw hotshotsxyz\/tailmon:dev tailmon /project $FILE_NAME "$2" $3
